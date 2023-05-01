e7e23acb5ce339b1cdaf293579e384d299894742
Gaurav Soni <120136122+gaurav8158@users.noreply.github.com>
2023-05-01 18:49:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 function insert_Row() {
     //Write your code here
-  
-  
+	let table =document.getElementById("sampleTable");
+	table.innerHTML+=`
+ <tr><td>Row1 cell1</td> 
+		<td>Row1 cell2</td></tr> 
+ `
 }
