71a3dce051d1f77ccac9cc8e036168a39c46563f
Gaurav Soni <120136122+gaurav8158@users.noreply.github.com>
2023-05-01 19:07:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
-let n=0;
 function insert_Row() {
-    //Write your code here
-	let table =document.getElementById("sampleTable");
-	n=3;
-	table.innerHTML += `
-  <tr>
-    <td>Row${n} cell1</td> 
-    <td>Row${n} cell2</td>
-  </tr>
-`;
+   var table = document.getElementById("sampleTable");
+
+// Insert a new row at the top of the table
+var newRow = table.insertRow(0);
+
+// Insert two cells into the new row
+var newCell1 = newRow.insertCell(0);
+var newCell2 = newRow.insertCell(1);
+
+// Set the content of the new cells
+newCell1.innerHTML = "New Cell1";
+newCell2.innerHTML = "New cell2";
 }
